691e18f738083a6f69650d2ee0a808e9e69718ec
Yash Garg <thisisyashgarg@gmail.com>
2025-02-01 21:42:28 +0530

    feat: src files

---
+++ prettier.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+	arrowParens: 'always',
+	bracketSameLine: false,
+	bracketSpacing: true,
+	embeddedLanguageFormatting: 'auto',
+	endOfLine: 'lf',
+	htmlWhitespaceSensitivity: 'css',
+	insertPragma: false,
+	jsxSingleQuote: false,
+	printWidth: 200,
+	proseWrap: 'preserve',
+	quoteProps: 'as-needed',
+	requirePragma: false,
+	semi: false,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'none',
+	useTabs: true,
+	vueIndentScriptAndStyle: false
+	// plugins: [require('prettier-plugin-tailwindcss')],
+	// tailwindConfig: require.resolve('./tailwind.config.cjs')
+}
